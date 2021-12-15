34a2bcff8e09a25cb2c96b6da432083d1e3801d5
Mai Anh Nam <canmotcaiten.tn95@gmail.com>
2021-12-15 14:37:06 +0700

    user

--- database/migrations/2021_12_15_073221_add_column_avatar_deleted_at_at_user_table.php
+++ database/migrations/2021_12_15_073221_add_column_avatar_deleted_at_at_user_table.php
@@ -15,7 +15,7 @@ class AddColumnAvatarDeletedAtAtUserTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('avater');
+            $table->string('avatar')->nullable();
             $table->softDeletes();
         });
     }
